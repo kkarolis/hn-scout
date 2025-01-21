9bc80af3a17ea95068d2dedbcb2ed73502611698
Karolis Kalantojus <karolis.kalantojus@gmail.com>
2025-01-20 23:30:18 -0800

    drop the navigation as it doesn't work well on phone

--- src/extension.ts
+++ src/extension.ts
@@ -1,212 +1,246 @@
 import { marshal, unMarshal } from './marshaler';
 
+class HNJobDecisionWidget {
+    private _savedMap: Map<string, string>;
+    private _localStorage: Storage;
+    private _clickCallback: (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => void;
+    private _allPosts: string[]; // we use this to navigate to "tail" to next post
+    private _month: string;
+    private _dateSpecifier: string;
+    private _storageKey: string;
+    private _storagePointerKey: string;
+
+    private readonly STORAGE_KEY_PREFIX: string = "hn-scout";
+    private readonly STORAGE_KEY_POINTER_PREFIX: string = "hn-scout-pointer";
+    private readonly HASH_KEY: string = "#hn-scout=";
+
+    constructor(localStorage: Storage, dateSpecifier: string, allPosts: string[], clickCallback: (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => void) {
+        this._localStorage = localStorage;
+        this._allPosts = allPosts;
+        this._dateSpecifier = dateSpecifier;
+        this._month = dateSpecifier.split('-')[1];
+        this._storageKey = `${this.STORAGE_KEY_PREFIX}-${this._dateSpecifier}`;
+        this._storagePointerKey = `${this.STORAGE_KEY_POINTER_PREFIX}-${this._month}`;
+
+        // clear old storage if it exists
+        const existingStorageKey: string | null = localStorage.getItem(this._storagePointerKey)
+        if (existingStorageKey !== null && existingStorageKey !== this._storageKey) {
+            localStorage.removeItem(existingStorageKey);
+        }
+
+        // logic for loading data from hash
+        const hash = window.location.hash;
+        if (hash.startsWith(this.HASH_KEY)) {
+            const compressedData = hash.substring(this.HASH_KEY.length);
+            this._savedMap = new Map(unMarshal(compressedData));
+            this.saveToLocalStorage();
+            window.location.hash = '';
+
+            // remove last # from the section
+            window.location.href = window.location.href.slice(0, -1);
+        } else {
+            // need to namespace to month of year.
+            this._savedMap = new Map(JSON.parse(localStorage.getItem(this._storageKey) as string || '[]'));
+        }
+
+        this._clickCallback = clickCallback;
+    }
+
+    private saveToLocalStorage(): void {
+        this._localStorage.setItem(this._storageKey, JSON.stringify(Array.from(this._savedMap.entries())));
+    }
+
+    public enable(document: Document) {
+        document.addEventListener('click', async (e) => {
+            if (this.canHandleClick(e)) {
+                await this.handleClick(e);
+            }
+        })
+    }
+
+    public hasStorageItem(key: string): boolean {
+        return this._savedMap.has(key);
+    }
+
+    public canHandleClick(e: Event): boolean {
+        return e.target instanceof HTMLElement && e.target.classList.contains('hn-scout');
+    }
+
+    private async copyShareLink(e: Event) {
+        const compressedData = marshal(this._savedMap);
+        window.location.hash = `hn-scout=${compressedData}`;
+        navigator.clipboard.writeText(window.location.href);
+    }
+
+    private clearAll(e: Event) {
+        this._savedMap.clear();
+        this.saveToLocalStorage();
+        window.location.reload();
+    }
+
+    private tail(e: Event) {
+        let target: string | undefined = this._allPosts.length > 0 ? this._allPosts[0] : undefined;
+        let previousJobId: string | undefined = target;
+        
+        // iterate backwards from last post and find job before one which
+        // has a decision. This will not work reliably as there are up-votes
+        // on some jobs which will move them to the top, but need to
+        // experiment, maybe it will be good enough in practice.
+        for (const jobId of [...this._allPosts].reverse()) {
+            if (this._savedMap.has(jobId)) {
+                target = previousJobId;
+                break;
+            }
+            previousJobId = jobId;
+        }
+        
+        if (target !== undefined) {
+            window.location.hash = target;
+        }
+    }
+
+    private async handleDecision(e: Event) {
+        const target: HTMLElement = e.target as HTMLElement;
+
+        const afterStatus: string = target.getAttribute('data-job-status') as string;
+        const jobId: string = target.getAttribute('data-job-id') as string;
+
+        const beforeStatus = this._savedMap.get(jobId);
+
+        // persist the change
+        if (beforeStatus !== afterStatus) {
+            this._savedMap.set(jobId, afterStatus);
+            this.saveToLocalStorage();
+
+            target.classList.add('selected');
+
+            // de-select the other element
+            if (beforeStatus !== undefined) {
+                const otherElement = document.querySelector(`a.hn-scout[data-job-status="${beforeStatus}"][data-job-id="${jobId}"]`) as HTMLElement;
+                otherElement.classList.remove('selected')
+            }
+        } else {
+            this._savedMap.delete(jobId);
+            this.saveToLocalStorage();
+            target.classList.remove('selected');
+        }
+
+        // update the UI
+        await this._clickCallback(e, jobId, beforeStatus, afterStatus);
+
+    }
+
+    public async handleClick(e: Event) {
+        if (!(e.target instanceof HTMLElement)) return;
+
+        if (e.target.classList.contains('hn-scout-copy-share-link')) {
+            await this.copyShareLink(e);
+            return;
+        }
+
+        if (e.target.classList.contains('hn-scout-clear')) {
+            this.clearAll(e);
+            return;
+        }
+
+        if (e.target.classList.contains('hn-scout-tail')) {
+            this.tail(e);
+            return;
+        }
+
+        await this.handleDecision(e);
+    }
+
+    private html(strings: TemplateStringsArray, ...values: any[]): string {
+        return String.raw({ raw: strings }, ...values);
+    }
+
+    // some good old brittle html with implied structure
+    public getHtmlToolbarElementText(): string {
+        return this.html`
+            <tr class="coll athing comtr" id="doesnotexist">
+                <td>
+                    <table border="0">
+                        <tbody>
+                            <tr>
+                                <td class="ind" indent="0"></td>
+                                <td valign="top" class="nosee votelinks" style="background-size: 10px;">
+                                    <center><div class="votearrow" style="background-image: none;"></div></center>
+                                </td>
+                                <td class="default">
+                                    <div style="margin-top:2px; margin-bottom:-10px;">
+                                        <span class="comhead">
+                                        HN Scout | 
+                                            <a href="javascript:void(0)" class="hn-scout hn-scout-copy-share-link">cp link</a>
+                                            |
+                                            <a href="javascript:void(0)" class="hn-scout hn-scout-clear">clear</a>
+                                            |
+                                            <a href="javascript:void(0)" class="hn-scout hn-scout-tail">tail</a>
+                                        <br/>
+                                        </span>
+                                    </div><br/>
+                                    <div class="noshow comment"><div class="commtext c00"></div></div>
+                                    <br/>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        `.trim();
+    }
+
+    public getHtmlPerCommentElementText(id: string): string {
+        return this.html`
+            <a href="#" class="hn-scout ${this._savedMap.get(id) === 'yes' ? 'hn-scout-selected' : ''}" 
+                data-job-id="${id}" data-job-status="yes">yes</a> |
+            <a href="#" class="hn-scout ${this._savedMap.get(id) === 'no' ? 'hn-scout-selected' : ''}" 
+                data-job-id="${id}" data-job-status="no">no</a> |
+            <a href="#" class="hn-scout ${this._savedMap.get(id) === 'maybe' ? 'hn-scout-selected' : ''}" 
+                data-job-id="${id}" data-job-status="maybe">maybe</a>
+        `.trim();
+    }
+}
+
 (() => {
     const selectorTopLevelComments = "td[indent='0'] + td + td";
 
-    class HNJobDecisionWidget {
-        private _savedMap: Map<string, string>;
-        private _localStorage: Storage;
-        private _clickCallback: (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => void;
-        private _allPosts: string[];
+    const isAuthoredByWhoIsHiring = () => {
+        return document.querySelector("table.fatitem a[href='user?id=whoishiring']") !== null;
+    }
 
-        private readonly STORAGE_KEY: string = "hn-job-decisions";
-        private readonly HASH_KEY: string = "#hn-job-decisions=";
-        
-        constructor(localStorage: Storage, allPosts: string[], clickCallback: (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => void) {
-            this._localStorage = localStorage;
-            this._allPosts = allPosts;
+    const isWhoIsHiringPost = () => {
+        const titleLine: HTMLElement = document.querySelector("table.fatitem span.titleline a") as HTMLElement;
+        return titleLine.textContent?.includes("Who is hiring?");
+    }
 
-            // logic for loading data from hash
-            const hash = window.location.hash;
-            if (hash.startsWith(this.HASH_KEY)) {
-                const compressedData = hash.substring(this.HASH_KEY.length);
-                this._savedMap = new Map(unMarshal(compressedData));
-                this.saveToLocalStorage();
-                window.location.hash = '';
+    // only install the widget on who is hiring posts
+    if (!isAuthoredByWhoIsHiring() || !isWhoIsHiringPost()) {
+        return;
+    }
 
-                // remove last # from the section
-                window.location.href = window.location.href.slice(0, -1);
-            } else {
-                this._savedMap = new Map(JSON.parse(localStorage.getItem(this.STORAGE_KEY) as string || '[]'));
-            }
-
-            this._clickCallback = clickCallback;
-        }
-
-        private saveToLocalStorage(): void {
-            this._localStorage.setItem(this.STORAGE_KEY, JSON.stringify(Array.from(this._savedMap.entries())));
-        }
-
-        public enable(document: Document) {
-            document.addEventListener('click', async (e) => {
-                if (this.canHandleClick(e)) {
-                    await this.handleClick(e);
-                }
-            })
-        }
-
-        public hasStorageItem(key: string): boolean {
-            return this._savedMap.has(key);
-        }
-
-        public canHandleClick(e: Event): boolean {
-            return e.target instanceof HTMLElement && e.target.classList.contains('hn-job-decision');
-        }
-
-        private async copyShareLink(e: Event) {
-            const compressedData = marshal(this._savedMap);
-            window.location.hash = `hn-job-decisions=${compressedData}`;
-            navigator.clipboard.writeText(window.location.href);
-        }
-
-        private clearAll(e: Event) {
-            this._savedMap.clear();
-            this.saveToLocalStorage();
-            window.location.reload();
-        }
-
-        private tail(e: Event) {
-            let target: string | undefined = this._allPosts.length > 0 ? this._allPosts[0] : undefined;
-            let previousJobId: string | undefined = target;
-            
-            // iterate backwards from last post and find job before one which
-            // has a decision. This will not work reliably as there are up-votes
-            // on some jobs which will move them to the top, but need to
-            // experiment, maybe it will be good enough in practice.
-            for (const jobId of [...this._allPosts].reverse()) {
-                if (this._savedMap.has(jobId)) {
-                    target = previousJobId;
-                    break;
-                }
-                previousJobId = jobId;
-            }
-            
-            if (target !== undefined) {
-                window.location.hash = target;
-            }
-        }
-
-        private async handleDecision(e: Event) {
-            const target: HTMLElement = e.target as HTMLElement;
-
-            const afterStatus: string = target.getAttribute('data-job-status') as string;
-            const jobId: string = target.getAttribute('data-job-id') as string;
-
-            const beforeStatus = this._savedMap.get(jobId);
-
-            // persist the change
-            if (beforeStatus !== afterStatus) {
-                this._savedMap.set(jobId, afterStatus);
-                this.saveToLocalStorage();
-
-                target.classList.add('selected');
-
-                // de-select the other element
-                if (beforeStatus !== undefined) {
-                    const otherElement = document.querySelector(`a.hn-job-decision[data-job-status="${beforeStatus}"][data-job-id="${jobId}"]`) as HTMLElement;
-                    otherElement.classList.remove('selected')
-                }
-            } else {
-                this._savedMap.delete(jobId);
-                this.saveToLocalStorage();
-                target.classList.remove('selected');
-            }
-
-            // update the UI
-            await this._clickCallback(e, jobId, beforeStatus, afterStatus);
-
-        }
-
-        public async handleClick(e: Event) {
-            if (!(e.target instanceof HTMLElement)) return;
-
-            if (e.target.classList.contains('copy-share-link')) {
-                await this.copyShareLink(e);
-                return;
-            }
-
-            if (e.target.classList.contains('clear')) {
-                this.clearAll(e);
-                return;
-            }
-
-            if (e.target.classList.contains('tail')) {
-                this.tail(e);
-                return;
-            }
-
-            await this.handleDecision(e);
-        }
-
-        private html(strings: TemplateStringsArray, ...values: any[]): string {
-            return String.raw({ raw: strings }, ...values);
-        }
-
-        // some good old brittle html with implied structure
-        public getHtmlToolbarElementText(): string {
-            return this.html`
-                <tr class="coll athing comtr" id="doesnotexist">
-                    <td>
-                        <table border="0">
-                            <tbody>
-                                <tr>
-                                    <td class="ind" indent="0"></td>
-                                    <td valign="top" class="nosee votelinks" style="background-size: 10px;">
-                                        <center><div class="votearrow" style="background-image: none;"></div></center>
-                                    </td>
-                                    <td class="default">
-                                        <div style="margin-top:2px; margin-bottom:-10px;">
-                                            <span class="comhead">
-                                            HN Job Actions | 
-                                                <a href="javascript:void(0)" class="hn-job-decision copy-share-link">cp link</a>
-                                                |
-                                                <a href="javascript:void(0)" class="hn-job-decision clear">clear</a>
-                                                |
-                                                <a href="javascript:void(0)" class="hn-job-decision tail">tail</a>
-                                            <br/>
-                                            </span>
-                                        </div><br/>
-                                        <div class="noshow comment"><div class="commtext c00"></div></div>
-                                        <br/>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                </tr>
-            `.trim();
-        }
-
-        public getHtmlPerCommentElementText(id: string): string {
-            return this.html`
-                <a href="#" class="hn-job-decision ${this._savedMap.get(id) === 'yes' ? 'selected' : ''}" 
-                   data-job-id="${id}" data-job-status="yes">yes</a> |
-                <a href="#" class="hn-job-decision ${this._savedMap.get(id) === 'no' ? 'selected' : ''}" 
-                   data-job-id="${id}" data-job-status="no">no</a> |
-                <a href="#" class="hn-job-decision ${this._savedMap.get(id) === 'maybe' ? 'selected' : ''}" 
-                   data-job-id="${id}" data-job-status="maybe">maybe</a>
-            `.trim();
-        }
+    function parseDate(date: string): string {
+        // Would have something like this 2024-10-01T15:00:16 1727794816
+        const isoDate: string = date.split(' ')[0];
+        const dateObj: Date = new Date(isoDate);
+        return `${dateObj.getFullYear()}-${(dateObj.getMonth() + 1).toString().padStart(2, '0')}`;
     }
 
     // install widget callback in the page
     const allPosts = Array.from(document.querySelectorAll(`${selectorTopLevelComments} span.navs a[id]`)).map((el) => el.id);
-    const hnJobDecisionWidget = new HNJobDecisionWidget(localStorage, allPosts, async (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => {
+    const dateSpecifier: string = parseDate((document.querySelector("table.fatitem span.age") as HTMLElement).title);
+
+
+    const hnJobDecisionWidget = new HNJobDecisionWidget(localStorage, dateSpecifier, allPosts, async (e: Event, jobId: string, beforeStatus: string | undefined, afterStatus: string) => {
         e.preventDefault();
+
         const toggleClose = () => (document.querySelector(`tr[id='${jobId}'] a[id='${jobId}']`) as HTMLElement).click();
-        const next = () => {
-            const nodeList: NodeListOf<HTMLElement> = document.querySelectorAll(`tr[id='${jobId}'] span.navs a.clicky`);
-            const linksToNext = Array.from(nodeList).filter((el) => el.textContent === 'next');
-            if (linksToNext.length > 0) {
-                linksToNext[0].click();
-            }
-        };
+
+        // handles the navigation logic
+        // some delicious mommas spaghetti right here 
         if (beforeStatus === undefined) {
             if (afterStatus === 'no') {
                 toggleClose();
-                next();
-            } else {
-                next();
             }
         } else {
             if (afterStatus === 'no') {
@@ -215,10 +249,10 @@ import { marshal, unMarshal } from './marshaler';
                 if (beforeStatus === 'no') {
                     toggleClose();
                 }
-                next();
             }
         }
     });
+
     hnJobDecisionWidget.enable(document);
 
     document.querySelector('table.comment-tree tbody')?.insertAdjacentHTML('afterbegin', hnJobDecisionWidget.getHtmlToolbarElementText());
@@ -228,6 +262,7 @@ import { marshal, unMarshal } from './marshaler';
         const jobId = el.id;
         let jobElements = hnJobDecisionWidget.getHtmlPerCommentElementText(jobId);
         el.insertAdjacentHTML('afterend', ` | ${jobElements}`);
+
         if (hnJobDecisionWidget.hasStorageItem(jobId)) {
             (document.querySelector(`tr[id='${jobId}'] a[id='${jobId}']`) as HTMLElement)?.click();
         }
--- tests-e2e/extension.test.ts
+++ tests-e2e/extension.test.ts
@@ -1,48 +1,79 @@
 import { test, expect } from './fixtures';
 
-test('widget hides comments when clicked and persists', async ({ page }) => {
+test('widget closes the negative decision.', async ({ page }) => {
+  const jobId = '42579330';
   await page.goto('https://news.ycombinator.com/item?id=42575537');
 
   // No comments are hidden initially.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(0);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
 
-  await page.click("tr[id='42579330'] a.hn-job-decision[data-job-status='yes']");
+  await page.click(`tr[id='${jobId}'] a.hn-scout[data-job-status='no']`);
 
   // Automatic closure of the child comments on click.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(1);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(1);
 
   await page.reload();
 
   // Change persists because of browser storage.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(1);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(1);
 });
 
-test('widget allows to toggle the decision without uncovering the comments', async ({ page }) => {
+test('widget allows to toggle the decision uncovering the post', async ({ page }) => {
+  const jobId = '42579330';
   await page.goto('https://news.ycombinator.com/item?id=42575537');
 
   // No comments are hidden initially.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(0);
-  await page.click("tr[id='42579330'] a.hn-job-decision[data-job-status='yes']");
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
 
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(1);
-  await page.click("tr[id='42579330'] a.hn-job-decision[data-job-status='no']");
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(1);
+  await page.click(`tr[id='${jobId}'] a.hn-scout[data-job-status='no']`);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(1);
+
+  await page.click(`tr[id='${jobId}'] a.hn-scout[data-job-status='yes']`);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
 });
 
-test('widget uncovers comments if same decision is un-select is persisted', async ({ page }) => {
+test('widget uncovers comments if same decision is un-selected', async ({ page }) => {
+  const jobId = '42579330';
   await page.goto('https://news.ycombinator.com/item?id=42575537');
 
   // No comments are hidden initially.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(0);
-  await page.click("tr[id='42579330'] a.hn-job-decision[data-job-status='yes']");
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(1);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
+
+  await page.click(`tr[id='${jobId}'] a.hn-scout[data-job-status='no']`);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(1);
 
   // Selecting same decision again uncovers the comments
-  await page.click("tr[id='42579330'] a.hn-job-decision[data-job-status='yes']");
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(0);
+  await page.click(`tr[id='${jobId}'] a.hn-scout[data-job-status='no']`);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
 
   await page.reload();
 
   // Change persists because of browser storage.
-  expect(await page.locator("tr[id='42579330'] td.nosee").count()).toBe(0);
+  expect(await page.locator(`tr[id='${jobId}'] td.nosee`).count()).toBe(0);
+});
+
+test('widget only is enabled on documents of who is hiring', async ({ page }) => {
+  // the who is hiring page
+  await page.goto('https://news.ycombinator.com/item?id=42575537');
+  expect(await page.locator('a.hn-scout').count()).toBeGreaterThan(0);
+
+  // a random page
+  await page.goto('https://news.ycombinator.com/item?id=42769871');
+  expect(await page.locator('a.hn-scout').count()).toBe(0);
+});
+
+test('widget correctly handles same month but different year posts', async ({ page }) => {
+  // old post from January 2024
+  const oldPostId = '38844766';
+  await page.goto('https://news.ycombinator.com/item?id=38842977');
+
+  await page.click(`tr[id='${oldPostId}'] a.hn-scout[data-job-status='no']`);
+  await page.click("a.hn-scout.hn-scout-copy-share-link");
+  const oldHash = await page.evaluate(() => { return window.location.hash });
+
+  // new post from January 2025
+  await page.goto('https://news.ycombinator.com/item?id=42575537');
+  await page.click("a.hn-scout.hn-scout-copy-share-link");
+  const newHash = await page.evaluate(() => { return window.location.hash });
+  expect(oldHash).not.toBe(newHash);
 });
